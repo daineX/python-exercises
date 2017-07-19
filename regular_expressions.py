06b8305c3ff2d39274796a64c0dbbf38c81de3d2
Paul Seidel <pse@ableton.com>
2017-07-19 16:00:00 +0200

    Add some more regular expression exercises

--- regular_expressions.py
+++ regular_expressions.py
@@ -17,8 +17,9 @@ rex = 'spam' # matches the string 'spam' exactly
 # Python defines all the functionality regarding regular expressions in the 're' module:
 import re
 
-# The 'match' function is used to see if string matches a regular expression:
+# The 'match' function is used to see if the beginning of a string matches a regular expression:
 assert re.match(rex, 'spam')
+assert re.match(rex, 'spammer')
 assert not re.match(rex, 'eggs')
 
 # If the string does not match the regular expression
@@ -106,6 +107,25 @@ assert re.search(rex, 'spamfooeggs')
 # assert not re.search(rex, 'road')
 # assert re.search(rex, 'toast')
 
+# Python also allows for checking for a specific number of occurrences. For this
+# '{}' (curly braces) are used:
+rex = 'ba{5}'
+assert re.match(rex, 'baaaaa')
+assert not re.match(rex, 'baaa')
+
+rex = 'ba{3,5}'
+assert re.match(rex, 'baaaaa')
+assert re.match(rex, 'baaa')
+assert not re.match(rex, 'ba')
+
+# EXERCISE:
+# Write a regular expression that matches three to six 'o's followed by 'uf':
+# rex = 'somerex'
+# assert re.match(rex, 'ooouf')
+# assert re.match(rex, 'oooooouf')
+# assert not re.match(rex, 'ouf')
+
+
 # For now we've only checked multiple occurrences of a single character. With grouping
 # we can do this for a sequence of characters putting them in parentheses:
 rex = "(eg)?gs"
@@ -116,9 +136,67 @@ assert re.match(rex, "eggs")
 # Notice how 'ggs' is not matched, because the 'eg' group can only be matched as one.
 
 # EXERCISE:
-# Write a regular expression that matches an arbitrary number of arbitrary characters
-# in the front but ends with either eggs or spam:
+# Write a regular expression that matches 'some', then an arbitrary number of arbitrary characters
+# in the middle and ends with either 'eggs' or 'spam':
 #rex = 'somerex'
-#assert re.match(rex, 'some eggs')
-#assert re.match(rex, 'delicious spam')
-#assert not re.match(rex, 'salty ham')
+#assert re.match(rex, 'some classy eggs')
+#assert re.match(rex, 'some delicious spam')
+#assert not re.match(rex, 'some salty ham')
+#assert not re.match(rex, 'any spammy spam')
+
+# The '|' (bar) can be used mutiple times:
+rex = 'a|b|c'
+assert re.match(rex, 'at home')
+assert re.match(rex, 'bar none')
+assert re.match(rex, 'cute kittens')
+assert not re.match(rex, 'dumb dogs')
+
+# But this can be become tedious if we want to match then just a few characters or patterns.
+# For this purpose, we can square braces as more compact way of writing these expressions:
+
+rex = '[abc]'
+assert re.match(rex, 'at home')
+assert re.match(rex, 'bar none')
+assert re.match(rex, 'cute kittens')
+assert not re.match(rex, 'dumb dogs')
+
+# Furthermore, we can use ranges of characters by putting dashes in between:
+rex = '[a-c]'
+assert re.match(rex, 'at home')
+assert re.match(rex, 'bar none')
+assert re.match(rex, 'cute kittens')
+assert not re.match(rex, 'dumb dogs')
+
+# For example, we can use [A-Z] to match all capital letters:
+rex = '[A-Z]'
+assert re.match(rex, 'Cute kittens')
+assert not re.match(rex, 'cute kittens')
+
+# EXERCISE:
+# Write a regular expression that matches any string containing a number from 1 to 9
+# rex = 'somerex'
+# assert re.match(rex, 'toaster 3 still works')
+# assert not re.match(rex, 'bread')
+# assert not re.match(rex, 'number 0 is gone')
+
+# We can also have multiple ranges in the same square brace expression:
+rex = '[A-Za-z]+'
+assert re.match(rex, 'immadeoutofletters')
+assert not re.match(rex, '77Istartwithanumber')
+
+# Python defines shortcuts for some of these ranges. You can use \d to match
+# numerals and \w to match numerals and letters and underscores:
+rex = '\d+\w*\d+' # match a string starting with a numeral, maybe followed by any
+                  # letter or number, then followed by at least a numeral again
+assert re.match(rex, '0spam3')
+assert not re.match(rex, '0 45')
+assert not re.match(rex, '0spam')
+
+# EXERCISE:
+# Write a regular expression, that matches 'there are x kittens in the kitchen',
+# with x being any number.
+rex = 'somerex'
+assert re.match(rex, 'there are 5 kittens in the kitchen')
+assert re.match(rex, 'there are 25 kittens in the kitchen')
+assert not re.match(rex, 'there are no kittens in the kitchen')
+assert not re.match(rex, 'there are kittens in the kitchen')
